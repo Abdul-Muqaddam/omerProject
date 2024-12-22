786dd6d82cf697b31c0478e2a1e9170a52e1e34d
Abdul-Muqaddam <abdul.muqaddam.se@gmail.com>
2024-12-23 00:45:47 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <>
+      <div className='bg-[#10141C] h-[100vh] w-[100vw]'>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="flex items-center justify-center h-[15vh]">
+                {/* <img src="#" alt="" /> */}
+                <div className="w-[88.39%] text-white h-[56px] rounded-[20px] bg-white bg-opacity-5 flex items-center justify-around ">
+                    <ul className="flex justify-between w-[35vw] ">
+                        <li className="flex gap-2 cursor-pointer ">
+                            <img src="/dot.svg" alt="" />
+                            <div className="text-[#BAD7F5] ">Home</div>
+                        </li>
+                        <li className="text-[#BAD7F5] cursor-pointer hover:text-[#80d1f6] ">
+                            Enterprise
+                        </li>
+                        <li className="text-[#BAD7F5] cursor-pointer hover:text-[#80d1f6] ">
+                            Expertise
+                        </li>
+                        <li className="text-[#BAD7F5] cursor-pointer hover:text-[#80d1f6] ">
+                            Pricing
+                        </li>
+                        <li className="text-[#BAD7F5] cursor-pointer hover:text-[#80d1f6] ">
+                            Contact
+                        </li>
+                    </ul>
+                    <ul className="flex justify-between w-[26vw]">
+                        <li className="flex items-center cursor-pointer group relative w-[6vw]">
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-0 group-hover:opacity-100 duration-300 transition-all ease-in-out"/>
+                            <div className="text-[#BAD7F5] absolute group-hover:left-4 left-0 transition-all duration-300 ease-in-out">About us</div>
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-100 group-hover:opacity-0 transition-all duration-300 ease-in-out absolute right-0"/>
+                        </li>
+                        <li className="flex items-center cursor-pointer group relative w-[4.7vw]">
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-0 group-hover:opacity-100 duration-300 transition-all ease-in-out"/>
+                            <div className="text-[#BAD7F5] absolute group-hover:left-4 left-0 transition-all duration-300 ease-in-out">Stacks</div>
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-100 group-hover:opacity-0 transition-all duration-300 ease-in-out absolute right-0"/>
+                        </li>
+                        <li className="flex items-center cursor-pointer group relative w-[8vw]">
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-0 group-hover:opacity-100 duration-300 transition-all ease-in-out"/>
+                            <div className="text-[#BAD7F5] absolute group-hover:left-4 left-0 transition-all duration-300 ease-in-out">Case Studies</div>
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-100 group-hover:opacity-0 transition-all duration-300 ease-in-out absolute right-0"/>
+                        </li>
+                        <li className="flex items-center cursor-pointer group relative w-[4vw]">
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-0 group-hover:opacity-100 duration-300 transition-all ease-in-out"/>
+                            <div className="text-[#BAD7F5] absolute group-hover:left-4 left-0 transition-all duration-300 ease-in-out">Docs</div>
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] opacity-100 group-hover:opacity-0 transition-all duration-300 ease-in-out absolute right-0"/>
+                        </li>
+                        
+                        
+                        
+                    </ul>
+                    <div className="flex gap-3">
+                        <button className="flex items-center group">
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] hidden group-hover:block" />
+                            <div className="text-[#BAD7F5]">Sign in</div>
+                            <img src="/arrow.png" alt="" className="h-[16px] w-[16px] group-hover:hidden" />
+                        </button>
+                        <button>
+                            <div className="text-black bg-[#BAD7F5] w-[130.61px] h-[42.76px] flex items-center justify-center rounded-[16px] text-[18px] hover:h-[40.76px] hover:w-[128.61px] transition-all duration-500 ease-in-out">
+                                Sign up
+                            </div>
+                            <img src="" alt="" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
