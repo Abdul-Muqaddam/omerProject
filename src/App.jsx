f15c1400b37bfbbb6249a86607d00fbfd3004da1
Abdul-Muqaddam <abdul.muqaddam.se@gmail.com>
2024-12-23 02:07:18 +0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/navbar'
 function App() {
   return (
     <>
-      <div className='bg-[#0f161f] h-[100vh] w-[100vw]'>
+      <div className='bg-[#0f161f] h-[100vh] w-[110vw]'>
         <Navbar/>
       </div>
     </>
